c507c1e72180135c17795deb6e885e74d3d4c961
Giel van Schijndel <me@mortis.eu>
2015-05-26 23:15:17 +0200

    Use the new env_logger and log crates for logging

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 /* vim: set et sts=4 sw=4: */
 
 #[macro_use] extern crate log;
+extern crate env_logger;
 
 #[cfg(not(test))]
 use rudoku::create_puzzle;
@@ -11,6 +12,8 @@ mod test;
 
 #[cfg(not(test))]
 fn main() {
+    env_logger::init().unwrap();
+
     for e_line in std::io::stdin().lock().lines() {
         match e_line {
             Ok(line) => {
