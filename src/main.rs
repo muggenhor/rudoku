ca433ee4a8aba4cd6f80f4cdde9a975cc005ee88
Giel van Schijndel <me@mortis.eu>
2015-05-27 00:25:59 +0200

    Just borrow a `string` instead of relying on unstable features

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 /* vim: set et sts=4 sw=4: */
 
 #![feature(collections)]
-#![feature(convert)]
 
 #[macro_use] extern crate log;
 extern crate env_logger;
@@ -23,7 +22,7 @@ fn main() {
     for e_line in stdin.lock().lines() {
         match e_line {
             Ok(line) => {
-                let mut cur_puzzle = create_puzzle(line.as_str());
+                let mut cur_puzzle = create_puzzle(&line);
                 println!("{}", cur_puzzle);
                 cur_puzzle.solve();
                 println!("{}", cur_puzzle);
